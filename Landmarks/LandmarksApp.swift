b6b90ad2a9beb93884c45cee95e9c29df34206db
Xuqian Zhang <zxuqian@gmail.com>
2023-10-08 11:25:42 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Xuqian Zhang on 2023/10/8.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
